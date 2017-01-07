b218510ba109679957fcc8e3bd86ca95d7bfc07d
okumin <mail@okumin.com>
2017-01-07 09:28:12 +0900

    Add setup scripts

--- setup_receiver.sh
+++ setup_receiver.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-RECEIVER_HOST=$1
-
 cd ~/influent-benchmark/ansible
 ./setup.sh
 
 cd ~/influent-benchmark/ansible
-ansible-playbook -i load sender.yml --extra-vars "receiver_host=${RECEIVER_HOST}"
+ansible-playbook -i load receiver.yml
 source ~/.bash_profile
