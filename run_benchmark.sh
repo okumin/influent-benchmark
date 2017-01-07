daac43e70bc78a993a7627dacebe511d991d1dd4
okumin <mail@okumin.com>
2017-01-07 11:00:08 +0900

    Add run_benchmark.sh

---
+++ run_benchmark.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -eu
+
+VERSION=$1
+MPS=$2
+
+rm /var/tmp/_var_log_dummy.pos
+rm ~/fluentd-benchmark-${VERSION}/in_forward/dummy.log
+
+cd ~/fluentd-benchmark-${VERSION}/in_forward
+bundle exec fluentd -c agent.conf &
+bundle exec dummer -c dummer.conf -r ${MPS}
