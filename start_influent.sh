78828a54a18783f5b976935e3df33aa709164a6d
okumin <mail@okumin.com>
2017-01-23 22:12:39 +0900

    Change the path of gc.log into absolute path

--- start_influent.sh
+++ start_influent.sh
@@ -9,4 +9,4 @@ cd ~/influent
 git checkout $TEST_BRANCH
 sbt "project influentJavaSample" "assembly"
 
-java -XX:+UseG1GC -Xmx16g -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:~/influent-log/gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=8 -XX:GCLogFileSize=16m -classpath influent-java-sample/target/influent-java-sample.jar sample.Counter
+java -XX:+UseG1GC -Xmx16g -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:${HOME}/influent-log/gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=8 -XX:GCLogFileSize=16m -classpath influent-java-sample/target/influent-java-sample.jar sample.Counter
