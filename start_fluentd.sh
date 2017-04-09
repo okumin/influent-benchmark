037d27d65628e5f2e611fdbcf7d4192e2e8d5d60
okumin <mail@okumin.com>
2017-04-09 21:40:02 +0900

    Add flowcounter_total.py

--- start_fluentd.sh
+++ start_fluentd.sh
@@ -4,4 +4,4 @@ VERSION=$1
 MODE=$2
 
 cd ~/fluentd-benchmark-${VERSION}-${MODE}/in_forward
-FLUENTD_WORKERS=$3 bundle exec fluentd -c receiver.conf
+FLUENTD_WORKERS=$3 bundle exec fluentd -c receiver.conf | python ~/script/flowcounter_total.py
