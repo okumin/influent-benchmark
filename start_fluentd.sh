c9cef095c6fc6c79dfb57aa7073d1f187ccbcbd3
okumin <mail@okumin.com>
2017-11-17 00:29:46 +0900

    Put jemalloc on home directory

--- start_fluentd.sh
+++ start_fluentd.sh
@@ -5,4 +5,4 @@ MODE=$2
 
 # There is no difference between at-most-once and at-least-once on receiver's side.
 cd ~/fluentd-benchmark-${VERSION}-at-least-once/in_forward
-FLUENTD_WORKERS=$3 LD_PRELOAD=/usr/local/lib/libjemalloc.so bundle exec fluentd -c receiver.conf | python ~/script/flowcounter_total.py
+FLUENTD_WORKERS=$3 LD_PRELOAD=~/jemalloc/lib/libjemalloc.so bundle exec fluentd -c receiver.conf | python ~/script/flowcounter_total.py
